0d02c201dcb6b160a29789f883aea089473f61b6
Lại Tiến Đức <94907773+Tienduc37y@users.noreply.github.com>
2024-04-24 16:49:14 +0000

    btap vòng for

--- index.php
+++ index.php
@@ -1,27 +1,19 @@
-<?php
-
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
-
-function sayHello($name) {
-	echo "Hello $name!";
-}
-
-?>
-
 <html>
 	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<title>Visual Studio Code Remote :: PHP</title>
 	</head>
 	<body>
-		<?php 
-		
-		sayHello('remote world');
-			
-		phpinfo(); 
-			
+		<h2>Nhóm 17</h2>
+		<?php
+		for ($i = 1; $i <= 200; $i++) {
+			if ($i % 2 == 0) {
+				echo '<span style="color: red; font-weight: bold;">' . $i . '</span> ';
+			} else {
+				echo '<span style="color: blue; font-style: italic;">' . $i . '</span> ';
+			}
+		}
 		?>
+		
 	</body>
 </html>
